078ea996c7031cc8f3c6de25b2962310852cf983
dwasinge <dwasinge@redhat.com>
2021-03-09 10:49:09 -0700

    health check log exceptions

--- src/main/java/com/redhat/labs/lodestar/health/GitApiHealthCheck.java
+++ src/main/java/com/redhat/labs/lodestar/health/GitApiHealthCheck.java
@@ -7,6 +7,8 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.redhat.labs.lodestar.rest.client.LodeStarGitLabAPIService;
 
@@ -14,6 +16,7 @@ import com.redhat.labs.lodestar.rest.client.LodeStarGitLabAPIService;
 @ApplicationScoped
 public class GitApiHealthCheck implements HealthCheck {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GitApiHealthCheck.class);
     private static final String NAME = "Git API";
 
     @Inject
@@ -27,6 +30,7 @@ public class GitApiHealthCheck implements HealthCheck {
             service.getVersion();
             return HealthCheckResponse.up(NAME);
         } catch (Exception e) {
+            LOGGER.warn("git api health check failed...{}", e.getMessage());
             return HealthCheckResponse.down(NAME);
         }
 
